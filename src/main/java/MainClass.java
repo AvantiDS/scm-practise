40aaeb05ebb84a3e4f9451ea32a8c460f33e414a
AvantiDS <avanti.suryavanshi@gmail.com>
2024-06-21 22:57:39 +0100

    Jira 124 - First changes made to Dummy Methods.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,4 +3,8 @@ public class MainClass {
     public void mainClassMethod(){
         System.out.println("Main class method");
     }
+
+    public void mainClassMethod1(){
+        System.out.println("Main class method 1");
+    }
 }
--- src/test/java/uk/axone/TestClass.java
+++ src/test/java/uk/axone/TestClass.java
@@ -9,4 +9,9 @@ public class TestClass {
         System.out.println("Test Class Method");
     }
 
+    @Test
+    public void testClassMethod1(){
+        System.out.println("Test Class Method 1");
+    }
+
 }
